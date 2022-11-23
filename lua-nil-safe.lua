6448fe6efb8172ddade73b1f18f61a289d542670
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2022-11-23 08:46:39 -0300

    Update lua-nil-safe.lua

--- lua-nil-safe.lua
+++ lua-nil-safe.lua
@@ -5,21 +5,20 @@ function nil_metatable:__newindex() return            end
 function nil_metatable:__ipairs()   return ipairs({}) end
 function nil_metatable:__pairs()    return pairs({})  end
 function nil_metatable:__call(...)  return ...        end
-
-function nil_metatable:__concat(v) return v end
-function nil_metatable:__unm(v)    return v end
-function nil_metatable:__add(v)    return v end
-function nil_metatable:__sub(v)    return v end
-function nil_metatable:__mul(v)    return v end
-function nil_metatable:__div(v)    return v end
-function nil_metatable:__idiv(v)   return v end
-function nil_metatable:__mod(v)    return v end
-function nil_metatable:__pow(v)    return v end
-function nil_metatable:__band(v)   return v end
-function nil_metatable:__bor(v)    return v end
-function nil_metatable:__bxor(v)   return v end
-function nil_metatable:__bnot(v)   return v end
-function nil_metatable:__shl(v)    return v end
-function nil_metatable:__shr(v)    return v end
+function nil_metatable:__concat(v)  return v end
+function nil_metatable:__unm(v)     return v end
+function nil_metatable:__add(v)     return v end
+function nil_metatable:__sub(v)     return v end
+function nil_metatable:__mul(v)     return v end
+function nil_metatable:__div(v)     return v end
+function nil_metatable:__idiv(v)    return v end
+function nil_metatable:__mod(v)     return v end
+function nil_metatable:__pow(v)     return v end
+function nil_metatable:__band(v)    return v end
+function nil_metatable:__bor(v)     return v end
+function nil_metatable:__bxor(v)    return v end
+function nil_metatable:__bnot(v)    return v end
+function nil_metatable:__shl(v)     return v end
+function nil_metatable:__shr(v)     return v end
 
 debug.setmetatable(nil,nil_metatable)
